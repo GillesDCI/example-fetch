ec931676a7a638985441bdb2bd2ba775540e54fd
Gilles <gilles.vanhaecke-freelancer@digitalcareerinstitute.org>
2022-03-03 13:32:34 +0100

    adding comments

--- src/routes/Listpage/Listpage.js
+++ src/routes/Listpage/Listpage.js
@@ -11,20 +11,23 @@ import Spinner from '../../components/common/Spinner';
 export default function Listpage(){
 
     //create state for the list page
-    const[items, setItems] = useState([]);
+    const[items, setItems] = useState([]); //initalize items as empty array
 
     //assigining navigation functionality
     const navigate = useNavigate();
 
     const loadData = async () => {
+
+        console.log("Length before the load is", items.length)
         const itemsFetched = await getAll();
-        setItems(itemsFetched);
+    
+        setItems(itemsFetched);    
     }
 
     useEffect(() => {
         loadData();
     }, [])
-    
+
 
 
     //itemsClicked function
@@ -40,7 +43,9 @@ export default function Listpage(){
         <div>
             <h1>Welcome to the list page!</h1>
               {/* if there are items disappear */}
-             <Spinner isVisible={items.length < 1} /> 
+             <Spinner isVisible={items.length < 1} />
+
+             { items.length < 1 ? <p>Length before is: {items.length}</p> :  <p>Length after load is: {items.length}</p>}
 
             <ul className="list-group">
                 {/* mapping through the list of items */}
@@ -48,7 +53,7 @@ export default function Listpage(){
                     //dynamically routing to the location using navigate
                     return <li key={item.id} className={`list-group-item ${styles.clickable}`} onClick={() => itemClicked(item)}>{item.itemText}</li>
 
-                    // alternative version using Link instead 
+                    // alternative version using Link instead
                     // return <Link to={`/list/${item.id}`}>{item.itemText}</Link>
                 })}
             </ul>
